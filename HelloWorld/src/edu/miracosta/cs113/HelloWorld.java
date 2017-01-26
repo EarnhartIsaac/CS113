5d8726dca2a8c36cf2a6a045af9a935c80e845c3
Isaac Earnhart <isaac.earnhart@gmail.com>
2017-01-25 17:15:16 -0800

    Initial Commit Hello World
    GitHuba and Eclipse test with a Hello World program

---
+++ HelloWorld/src/edu/miracosta/cs113/HelloWorld.java
@@ -0,0 +1,10 @@
+package edu.miracosta.cs113;
+
+public class HelloWorld 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Helpo Woglrd");
+	}
+}
---
+++ HelloWorld/src/edu/miracosta/cs113/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains source code for hello world demo
+ */
+/**
+ * @author W7262233
+ *
+ */
+package edu.miracosta.cs113;
